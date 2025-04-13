aba01d930073038ef8bd0821bd32a66fc5585f56
Nikolay <koly201458@gmail.com>
2025-04-14 00:32:51 +0300

    Add docker-file

--- LetterEater/Program.cs
+++ LetterEater/Program.cs
@@ -1,16 +1,24 @@
+using LetterEater.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<LetterEaterDbContext>(
+        options =>
+        {
+            options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(LetterEaterDbContext)));
+        });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
